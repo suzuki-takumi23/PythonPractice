03c6dda62f39546045dfd4e86f9b722ff778b862
goshadees <ms.gatx105freedom@gmail.com>
2025-04-12 18:42:19 +0900

    studied datetime and calendar module

--- DateAndTimePractice.py
+++ DateAndTimePractice.py
@@ -1,5 +1,6 @@
 #datetimeモジュールをインポート
 import datetime
+import calendar
 
 #変数todayを宣言
 #datetimeモジュールのdateクラスにあるtoday関数を使う
@@ -43,3 +44,29 @@ print(todaydetail.microsecond)
 print('------------------------------------------')
 print(today.isoformat())
 print(todaydetail.strftime("%Y/%m/%d %H:%M:%S"))
+
+#日付の計算
+today = datetime.datetime.today()
+
+#今日の日付
+print(today)
+
+#明日の日付
+print(today + datetime.timedelta(days=1))
+
+newyear = datetime.datetime(2010, 1, 1)
+
+#2010年1月1日の一週間後
+print(newyear + datetime.timedelta(days=7))
+
+#2010年1月1日から今日までの日数
+calc = today - newyear
+
+#計算結果の戻り値はtimedelta
+print(calc.days)
+
+print(calendar.isleap(2015))
+print(calendar.isleap(2016))
+print(calendar.isleap(2017))
+
+print(calendar.leapdays(2010, 2020))
