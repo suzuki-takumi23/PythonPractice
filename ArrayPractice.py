b0dd5473bbd97e9a7e8db7b92efff070c2e784c1
goshadees <ms.gatx105freedom@gmail.com>
2025-04-12 19:43:46 +0900

    add ArrayPractice commentout

--- ArrayPractice.py
+++ ArrayPractice.py
@@ -1,12 +1,14 @@
 import datetime
-
+#メンバ関数を定義
 def get_today():
 
     today = datetime.datetime.today()
+    
+    #タプル(配列)は()
     value = (today.year, today.month, today.day)
     
     return value
-
+#関数を使用
 test_tuple = get_today()
 
 print(test_tuple)
